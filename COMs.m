a02fb0d0d486bade0bb37deed5fb17882d624146
li-wen-hui <liwenhui@henu.edu.cn>
2026-07-21 20:33:46 +0800

    Update COMs.m

--- COMs.m
+++ COMs.m
@@ -6,7 +6,7 @@ T =40; % max_order
 MMT = PHFM_func(img, T);
 I_rec = PHFM_reconstruct_func(size(img,1), MMT);
 figure; subplot(1,2,1); imshow(img); subplot(1,2,2); imshow(uint8(abs(I_rec))); 
-
+ 
 function I_rec = PHFM_reconstruct_func(N, MMT)
     [X, Y] = meshgrid(linspace(-1, 1, N), linspace(1, -1, N));
     [theta, r] = cart2pol(X, Y);
